ef6b831531782bf53d6794e6eac7ea823651453e
sleepychloe <hyj2132@gmail.com>
2023-07-10 23:02:42 +0200

    20230710_23:02

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 # if [ ! -f "/setup.sql" ]; then
-
 /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+while ! mysqladmin ping --silent; do
+	sleep 1
+done
+
+touch root_pw.sql
+
+echo "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ADMIN_PW') WHERE User='root' ;" >> root_pw.sql
+echo "DELETE FROM mysql.user WHERE User='' ;" >> root_pw.sql
+echo "DELETE FROM mysql.user WHERE User=$MYSQL_ADMIN_USER' AND Host NOT IN ('localhost', '127.0.0.1', '::1') ;" >> root_pw.sql
+echo "DROP DATABASE IF EXISTS test ;" >> root_pw.sql
+echo "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%' ;" >> root_pw.sql
+echo "FLUSH PRIVILEGES ;" >> root_pw.sql
+
+mysql -u root < root_pw.sql
+
 
 touch setup.sql
 
@@ -10,18 +25,15 @@ echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME ;" >> setup.sql
 echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW' ;" >> setup.sql
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PW' WITH GRANT OPTION ;" >> setup.sql
 echo "FLUSH PRIVILEGES ;" >> setup.sql
-echo "ALTER USER '$MYSQL_ADMIN_USER'@'localhost' IDENTIFIED BY '$MYSQL_ADMIN_PW' ;" >> setup.sql
-
-#pkill mysqld
-# fi
 
 
-service mysql start
+mysql -u $MYSQL_ADMIN_USER -p$MYSQL_ADMIN_PW < setup.sql
 
-# myslqd < setup.sql
-mysql -u$MYSQL_ADMIN_USER -p$MYSQL_ADMIN_PW < setup.sql
+while ! mysqladmin ping --silent; do
+	sleep 1
+done
+#fi
 
 mysqld
-# /usr/bin/mysqld_safe --datadir=/var/lib/mysql < setup.sql
+
 /usr/bin/mysqld_safe --datadir=/var/lib/mysql
-# mysqld
